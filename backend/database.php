293ab3bff13985779fe1a6a14361d9ea662e817c
Juanchu <114174084+jmarpla2212@users.noreply.github.com>
2023-04-24 21:30:44 +0200

    Add files via upload

---
+++ backend/database.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "XXX.xxx.XXX.xxx";
+$username = "++++";
+$password = "++++";
+$dbname = "crud";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
